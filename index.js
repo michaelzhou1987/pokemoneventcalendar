bd9431b5deaade82de3842cdd58d4350f35c6b08
Michael Yizhuo Zhou <michaelzhouyizhuo1987@gmail.com>
2024-07-20 11:00:29 -0500

    update

--- index.js
+++ index.js
@@ -5,6 +5,66 @@ const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
 
+// example of calenders
+const calenders = [
+	{
+		shareableUrl: '',
+		calenderId: '',
+		timeOffSet: 5,
+		timeZone: 'America/Chicago',
+		city: 'Austin',
+		url: 'latitude=30.267153&longitude=-97.7430608&distance=100',
+	},
+	{
+		shareableUrl: '',
+		calenderId: '',
+		timeOffSet: 5,
+		timeZone: 'America/Chicago',
+		city: 'San Antonio',
+		url: 'latitude=29.4251905&longitude=-98.4945922&distance=100',
+	},
+	{
+		shareableUrl: '',
+		calenderId: '',
+		timeOffSet: 5,
+		timeZone: 'America/Chicago',
+		city: 'Houston',
+		url: 'latitude=29.7600771&longitude=-95.37011079999999&distance=100',
+	},
+	{
+		shareableUrl: '',
+		calenderId: '',
+		timeOffSet: 5,
+		timeZone: 'America/Chicago',
+		city: 'Dallas',
+		url: 'latitude=32.7766642&longitude=-96.79698789999999&distance=100',
+	},
+	{
+		shareableUrl: '',
+		calenderId: '',
+		timeOffSet: 5,
+		timeZone: 'America/Chicago',
+		city: 'Fort Worth',
+		url: 'latitude=32.7554883&longitude=-97.3307658&distance=100',
+	},
+	{
+		shareableUrl: '',
+		calenderId: '',
+		timeOffSet: 5,
+		timeZone: 'America/Chicago',
+		city: 'Tyler',
+		url: 'latitude=32.3512601&longitude=-95.30106239999999&distance=100',
+	},
+	{
+		shareableUrl: '',
+		calenderId: '',
+		timeOffSet: 5,
+		timeZone: 'America/Chicago',
+		city: 'College Station',
+		url: 'atitude=30.627977&longitude=-96.3344068&distance=100',
+	},
+];
+
 async function asyncForEach(array, callback) {
 	for (let index = 0; index < array.length; index++) {
 		await callback(array[index], index, array);
@@ -70,11 +130,15 @@ const isChallengeOrCup = (name) => {
 	return name.toLowerCase().includes('challenge') || name.toLowerCase().includes('cup');
 };
 
-const getPokemonEvents = async (auth) => {
+const fixTimeIssue = (time, offset) => {
+	// Adjust for timezone
+	return new Date(time).getTime() + offset * 60 * 60 * 1000;
+};
+
+const getPokemonEvents = async (auth, url, timeOffSet) => {
 	const {data} = await axios({
 		method: 'get',
-		// modify the url to get events in your area
-		url: `https://op-core.pokemon.com/api/v2/event_locator/search/?latitude=30.267153&longitude=-97.7430608&distance=100`,
+		url: `https://op-core.pokemon.com/api/v2/event_locator/search/?${url}`,
 	});
 
 	let events = data.activities;
@@ -83,7 +147,7 @@ const getPokemonEvents = async (auth) => {
 	});
 
 	const result = events.map((event) => {
-		const adjustedTime = new Date(event.start_datetime).getTime() + 5 * 60 * 60 * 1000; // Adjust for timezone
+		const adjustedTime = fixTimeIssue(event.start_datetime, timeOffSet);
 
 		return {
 			summary: event.name,
@@ -100,15 +164,37 @@ const getPokemonEvents = async (auth) => {
 		};
 	});
 
-	return [result, auth];
+	return result;
 };
 
-const addEvent = async (event, auth) => {
+async function listEvents(auth, calendarId) {
+	const calendar = google.calendar({version: 'v3', auth});
+
+	const res = await calendar.events.list({
+		calendarId: calendarId,
+		timeMin: new Date().toISOString(),
+		// maxResults: 10,
+		singleEvents: true,
+		orderBy: 'startTime',
+	});
+	const events = res.data.items;
+	if (!events || events.length === 0) {
+		// console.log('No upcoming events found.');
+		return [];
+	}
+
+	return events.map((event) => event.description);
+}
+
+const filterEvents = (events, existingEvents) => {
+	return events.filter((event) => !existingEvents.includes(event.description));
+};
+
+const addEvent = async (event, auth, calendarId) => {
 	calendar.events.insert(
 		{
 			auth,
-			// modify the calendarId to your calendar id
-			calendarId: '',
+			calendarId: calendarId,
 			resource: event,
 		},
 		function (err, event) {
@@ -116,17 +202,25 @@ const addEvent = async (event, auth) => {
 				console.log('There was an error contacting the Calendar service: ' + err);
 				return;
 			}
-			console.log('Event created: %s', event.data.start);
+			// console.log('Event created: %s', event.data.start);
 		}
 	);
 };
 
-authorize()
-	.then((auth) => {
-		getPokemonEvents(auth).then(([events, auth]) => {
-			asyncForEach(events, async (event, index) => {
-				await addEvent(event, auth, index);
-			});
+const run = async () => {
+	const auth = await authorize();
+	asyncForEach(calenders, async (calender) => {
+		const {calenderId, timeOffSet, city, url} = calender;
+		const existingEvents = await listEvents(auth, calenderId);
+		const eventsInArea = await getPokemonEvents(auth, url, timeOffSet);
+		const newEvents = filterEvents(eventsInArea, existingEvents);
+
+		asyncForEach(newEvents, async (event) => {
+			await addEvent(event, auth, calenderId);
 		});
-	})
-	.catch(console.error);
+
+		console.log(`${newEvents.length} events added for ${city}`);
+	});
+};
+
+run();
